0076083298da63fcd0dafe82ca381f194ffd3b50
John Jorgensen <john@leprechaun.dev>
2025-05-01 21:45:50 -0600

    add an identifier finder

--- packages/core/src/ast.ts
+++ packages/core/src/ast.ts
@@ -15,3 +15,30 @@ export function loadProgram(code: string): any {
     hashTree(sansLoc);
     return sansLoc;
 }
+
+export function getNodeByIdentifier(identifier: string, node: any) {
+    if (identifier === "") {
+        return node;
+    }
+    const identifiers = identifier.split(".");
+
+    if (typeof node === "object" && node !== null) {
+        const keysToCheck = [];
+        for (const key in node) {
+            if (typeof node[key] === "object" && node[key] !== null && node[key].type === "Identifier" && node[key].name === identifiers[0]) {
+                return getNodeByIdentifier(identifiers.slice(1).join("."), node);
+            }
+            else if (typeof node[key] === "object" && node[key] !== null) {
+                keysToCheck.push(key);
+            }
+        }
+
+        for (const key of keysToCheck) {
+            const result: any = getNodeByIdentifier(identifiers.join("."), node[key]);
+            if (result !== undefined) {
+                return result
+            }
+        }
+    }
+    return undefined;
+}
